0c937b88dcfed2af09ffc6d573cbecb33e3bfa30
Agnish07 <142070648+Agnish07@users.noreply.github.com>
2025-01-19 22:19:47 +0530

    Time: 0 ms (100%), Space: 11.4 MB (49.87%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> temp(n, 0);
+
+        for(int i = 0; i < heights.size();i++){
+            temp[i] = heights[i];
+        }
+
+        sort(temp.begin(), temp.end());
+
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(temp[i] != heights[i]){
+                count++;
+            }
+        }
+    return count;
+    }
+};
