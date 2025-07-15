216e89c9e44ebecf705e5498e4d323f380382bd0
Agnish07 <142070648+Agnish07@users.noreply.github.com>
2025-07-15 10:33:22 +0530

    Time: 4 ms (59.73%), Space: 15.8 MB (42.07%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -0,0 +1,104 @@
+class Solution {
+public:
+    vector<int> findnse(vector<int> &arr){
+        int n = arr.size();
+        vector<int> nse(n);
+        stack<int> st;
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            nse[i] = st.empty() ? n : st.top(); 
+            st.push(i);
+        }
+        return nse;
+    }
+
+     vector<int> findpsee(vector<int> &arr){
+        int n = arr.size();
+        vector<int> psee(n);
+        stack<int> st;
+
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
+            psee[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return psee;
+    }
+
+    vector<int> findnge(vector<int> &arr){
+        int n = arr.size();
+        vector<int> nse(n);
+        stack<int> st;
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                st.pop();
+            }
+            nse[i] = st.empty() ? n : st.top(); 
+            st.push(i);
+        }
+        return nse;
+    }
+
+     vector<int> findpgee(vector<int> &arr){
+        int n = arr.size();
+        vector<int> psee(n);
+        stack<int> st;
+
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && arr[st.top()] < arr[i]){
+                st.pop();
+            }
+            psee[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return psee;
+    }
+
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        int mod = 1e9 + 7;
+
+        vector<int> nse = findnse(arr);
+        vector<int> psee = findpsee(arr);
+
+        long long total = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            long long left = i - psee[i];
+            long long right = nse[i] - i;
+
+            total = (total + (left * right) * (long long)arr[i]);
+        }
+
+        return total;
+    }
+
+    long long sumSubarrayMaxs(vector<int>& arr) {
+        int n = arr.size();
+        int mod = 1e9 + 7;
+
+        vector<int> nse = findnge(arr);
+        vector<int> psee = findpgee(arr);
+
+        long long total = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            long long left = i - psee[i];
+            long long right = nse[i] - i;
+
+            total = (total + (left * right) * (long long)arr[i]);
+        }
+
+        return total;
+    }
+
+    long long subArrayRanges(vector<int>& nums) {
+        return (sumSubarrayMaxs(nums) - sumSubarrayMins(nums));
+    }
+};
