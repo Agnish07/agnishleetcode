4c41705899643199dc8e3b5d3d8d10f3d6d42ad2
Agnish07 <142070648+Agnish07@users.noreply.github.com>
2025-10-12 18:34:41 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int left = 0 ; int right = nums.size() - 1;
-        // if(nums[left] == target){
-        //     return left;
-        // }
-        // else if (nums[right] == target){
-        //     return right;
-        // }
+    int search(vector<int>& nums, int k) {
+        int low = 0;
+        int high = nums.size()-1;
 
-        while(left <= right){
-            int mid = (left + right) / 2;
+        while(low <= high){
+            int mid = low + (high-low)/2;
 
-            if(target == nums[mid]){
-                return mid;
-            }
+            if(nums[mid] == k) return mid;
 
-            if(nums[left] <= nums[mid]){
-                if(target > nums[mid] || target < nums[left]){
-                    left = mid + 1;
-                }
-            else{
-                right = mid - 1;}
-            }
-            else{
-                if(target < nums[mid] || target > nums[left]){
-                    right = mid - 1;
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= k && k < nums[mid]){
+                    high = mid-1;
                 }
                 else{
-                    left = mid + 1;
+                    low = mid + 1;
                 }
             }
 
+            else{
+                if(nums[mid] < k && nums[high] >= k){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
         }
+
         return -1;
     }
 };
