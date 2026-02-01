45345a1339ed152aab098ba97918383f1f11a861
Agnish07 <142070648+Agnish07@users.noreply.github.com>
2026-02-01 08:49:26 +0530

    Time: 0 ms (100%), Space: 30.5 MB (48.24%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        int x = nums[0];
-        if(nums.size() == 3) return nums[0]+nums[1]+nums[2];
-        sort(nums.begin()+1, nums.end());
-        int y = nums[1] + nums[2];
-        // if(nums[0] == x) y = nums[1] + nums[2];
-        // else if(nums[1] == x) y = nums[0] + nums[2];
-        // else y = nums[0]+nums[1];
-        return x+y;
+        // int x = nums[0];
+        // if(nums.size() == 3) return nums[0]+nums[1]+nums[2];
+        // sort(nums.begin()+1, nums.end());
+        // int y = nums[1] + nums[2];
+        // return x+y;
+        int cost = nums[0];
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+
+        for(int i = 1 ; i < nums.size();i++){
+            int cur = nums[i];
+            if(cur < min1){
+                min2 = min1;
+                min1 = cur;
+            }else if(min2 > cur){
+                min2 = cur;
+            }
+        }
+        cost += min1 + min2;
+        return cost;
     }
 };
