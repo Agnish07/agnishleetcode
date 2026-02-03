eed1de99018e35a57ab0daecafba9a48d589c104
Agnish07 <142070648+Agnish07@users.noreply.github.com>
2026-02-03 12:16:46 +0530

    Time: 0 ms (100%), Space: 23.6 MB (82.93%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-       int cnt1 = 0; int cnt2 = 0; int el1; int el2;
-       vector<int> ans;
+        int i = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int el1 = 0, el2 = 1;
+        int n = nums.size();
+        while(i < n){
+            if(nums[i] == el1) cnt1++;
+            else if(nums[i] == el2) cnt2++;
 
-       for(int i = 0 ; i < nums.size(); i++){
-        if(cnt1 == 0 && nums[i] != el2){
-            cnt1 = 1;
-            el1 = nums[i];
+            else if(cnt1 == 0){
+                el1 = nums[i];
+                cnt1++;
+            }
+            else if(cnt2 == 0){
+                el2 = nums[i];
+                cnt2++;
+            }
+
+
+            else{
+                cnt1--;
+                cnt2--;
+            }
+            i++;
         }
-        else if(cnt2 == 0 && nums[i] != el1){
-            cnt2 = 1;
-            el2 = nums[i];
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for(auto it : nums){
+            if(it == el1) cnt1++;
+            if(it == el2) cnt2++;
         }
-        else if(nums[i] == el1) cnt1++;
-        else if(nums[i] == el2) cnt2++;
-        else{
-            cnt1--;
-            cnt2--;
-        }
-       }
-        cnt1 = 0; cnt2 = 0;
-       for(int i = 0; i < nums.size(); i++){
-        if(nums[i] == el1){
-            cnt1++;
-        }
-        else if(nums[i] == el2){
-            cnt2++;
-        }
-       }
-       if(cnt1 >= (nums.size()/3) + 1){
-        ans.push_back(el1);
-       }
-        if(cnt2 >= (nums.size()/3) + 1){
-        ans.push_back(el2);
-       }
-       return ans;
+
+        vector<int> ans;
+        if(cnt1 > n/3) ans.push_back(el1);
+        if(cnt2 > n/3) ans.push_back(el2);
+
+        return ans;
+
     }
 };
