ecf7fd930005511d2674aed90ae46305b77f067a
Agnish07 <142070648+Agnish07@users.noreply.github.com>
2026-02-17 09:35:31 +0530

    Time: 0 ms (100%), Space: 9.5 MB (85.28%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+        for(int i = 0; i < 12; i++){
+            for(int j = 0; j < 60; j++){
+                if(__builtin_popcount(i) + __builtin_popcount(j) == turnedOn){
+                    string hr = to_string(i);
+                    string min = (j < 10 ? "0" : "") + to_string(j);
+
+                    ans.push_back(hr + ":" + min);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
