10b12a39eaeb4fc09f59cf58d2cd76235a9afb49
Agnish07 <142070648+Agnish07@users.noreply.github.com>
2026-02-28 11:48:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        dahdkhdddkdhkdkakdhkdkdhkhddhakdkhkahdkhhdkhdkdkhkhdkhdkhkhdkahdkhdkahdkhkshkhdkdhakhdkhdkahdkahdkahkhdkhdkdhhdhdkhkHShskhdkSKhdahdkhhkHKHSKHKSHKhsdkshkhSKHKHKdhkahkdhkhkskhKSHkkHDK
+        
+    }
+};
